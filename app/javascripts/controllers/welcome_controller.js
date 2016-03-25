cc59718ae2b9f96eab3e5bca3ede519dcce21426
Richard Kimber <richard@dogma.co.uk>
2016-03-25 14:36:15 +0000

    Smartened up, working on device

--- app/javascripts/controllers/welcome_controller.js
+++ app/javascripts/controllers/welcome_controller.js
@@ -6,10 +6,11 @@ export class WelcomeController extends StretchyHeaderController {
 
 	@route('welcome')
 	async index(): Promise {
-		const $html: Object = await this.render('welcome/index', {});
+		await this.render('welcome/index', {});
+
+		const imageHeight = 200;
 
 		let _scrollTop;
-		let _gestureStart;
 
 		$('.image-stretch')
 			.on('webkitTransitionEnd transitionend', function() {
@@ -31,23 +32,36 @@ export class WelcomeController extends StretchyHeaderController {
 
 				if (scrollTop <= 0) {
 					$imageStretch
-						.height(200 + Math.abs(scrollTop));
+						.height(imageHeight + Math.abs(scrollTop));
+
+					if (scrollTop === 0) {
+						$imageStretch
+							.css('transform', `translateY(0px)`);
+					}
+				}
+				else if (scrollTop > 0 && scrollTop <= imageHeight) {
+					$imageStretch
+						.css('transform', `translateY(-${scrollTop / 4}px)`);
+				}
+				else {
+					$imageStretch
+						.css('transform', `translateY(0px)`);
 				}
 			})
-			.on('touchstart', function(evt) {
-				_gestureStart = $(this).scrollTop();
-			})
 			.on('touchend', function() {
-				const scrollTop = $(this).scrollTop();
+				const scrollTop = $(this).scrollTop(),
+					$imageStretch = $('.image-stretch');
 
 				if (scrollTop < 0) {
-					const $imageStretch = $('.image-stretch');
-
 					$imageStretch
 						.addClass('smooth-operator');
 
 					$imageStretch
-						.height(200);
+						.height(imageHeight);
+				}
+				else if (scrollTop === 0 || scrollTop >= imageHeight) {
+					$imageStretch
+						.css('transform', `translateY(0px)`);
 				}
 			});
 	}
